8ce053c157504578dbe0193634f7f208bdfd014a
Andy Fehrenbach <andy.fehrenbach@gmail.com>
2021-03-14 23:27:26 -0500

    mobile site finished

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./pull-tab.svg":[["pull-tab.6766a4eb.svg","pull-tab.svg"],"pull-tab.svg"],"./line-art-02.svg":[["line-art-02.a2603762.svg","line-art-02.svg"],"line-art-02.svg"],"./round-blob.svg":[["round-blob.79a38d87.svg","round-blob.svg"],"round-blob.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./line-art-02.svg":[["line-art-02.a2603762.svg","line-art-02.svg"],"line-art-02.svg"],"./round-blob.svg":[["round-blob.79a38d87.svg","round-blob.svg"],"round-blob.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
