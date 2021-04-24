3605558d27fe02d6b835d1577c224a41c7525726
Andy Fehrenbach <andrew.fehrenbach@ampf.com>
2021-04-23 22:53:09 -0500

    project page top template laid out

--- scripts/adel.js
+++ scripts/adel.js
@@ -1,6 +1,6 @@
 const adelPage = document.getElementById('adelPage')
 const attemptedPassword = prompt('Please enter Password');
-console.log(attemptedPassword);
+
 if (attemptedPassword == 'Guest@2021!') {
     adelPage.classList.remove('hidden');
 } else {
